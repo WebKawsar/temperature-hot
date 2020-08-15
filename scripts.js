0e85acd3c67a4bda3eb67d488190f74a7fd5038d
WebKawsar <web.kawsarahmed@gmail.com>
2020-08-15 16:20:01 +0600

    get data from api

---
+++ scripts.js
@@ -0,0 +1,54 @@
+
+// function enter(){
+
+
+
+// }
+
+
+
+// if(){
+
+// }
+
+
+// document.getElementById("place").addEventListener("keypress", function(event){
+
+//     if(event.keyCode == 13){
+//         event.preventDefault();
+//     }
+// });
+
+
+
+
+document.getElementById("search").addEventListener("click", function(){
+
+    
+    let city = document.getElementById("place").value;
+    const key = "3369900f5b58c49516413f8ecdb9439d";
+    const base= "https://api.openweathermap.org/data/2.5/"
+    const url = `${base}weather?q=${city}&appid=${key}&units=metric`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const {weather} = data;
+        const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+        
+        document.getElementById("icon").src = icon;
+        name = document.getElementById("city-name").innerText = data.name;
+        document.getElementById("temperature").innerText = Math.round(data.main.temp) + "°C";
+        document.getElementById("clouds").innerText = data.weather[0].description;
+        document.getElementById("place").value = ""; 
+
+    })
+    // .catch((error) => {
+    //     document.getElementById("place").value = "Please search for a valid city 😩";
+    //     console.log(error);
+    // })
+
+});
+
+
